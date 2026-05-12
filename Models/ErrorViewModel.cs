5a91bf99ef101437c83bdbbf74254e5f58997f0c
TOMAS  CHESKIS <49551586@ort>
2026-05-12 09:04:34 -0300

    1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Recetify.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Models/Receta.cs
@@ -0,0 +1,8 @@
+Public class Receta()
+{
+    public string NombreChef{get; set;}
+    public DateTime FechaN{get; set;}
+    public string TipoC{get; set;}
+    public double Presupuesto{get; set;}
+    public int Comensales{get; set;}
+}
